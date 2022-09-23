5e2bf0a8fc365074eaa59f56a383c94a345a8a6f
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-23 15:23:47 +0000

    cumulative cases

--- .ccls-cache/@home@runner@ImportingFileUsingWhileLoop/main.cpp
+++ .ccls-cache/@home@runner@ImportingFileUsingWhileLoop/main.cpp
@@ -4,6 +4,7 @@ Test loading a .dat file.***/
 #include <fstream>
 #include<string>
 #include<iomanip>
+
 using namespace std;
 
 
@@ -12,6 +13,8 @@ int main() {
   string firstName;
   string date;
   int TestResult;
+  int count =0;
+  int cumulative_cases= 0;
   
   //declare stream varriables
   ifstream inFile; //needed to access the file we want to load
@@ -24,5 +27,23 @@ int main() {
   //read in the date -first line of file
   inFile >> date;
   cout << date <<endl;
+
+  //read in the 2 pieces of info on the secound line
+  inFile >>firstName;
+  cout<< firstName<< ' ';
+  inFile >>TestResult;
+  cout<< TestResult << endl;
+
+  while(inFile)
+    {
+      //update number of cases & persons tested
+      cumulative_cases = cumulative_cases + TestResult;
+      count++; //increment the number of patients
+
+      //read in the next line
+      inFile >> firstName;
+      inFile >> TestResult;
+    }
+  
   
 }
